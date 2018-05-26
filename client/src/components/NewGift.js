55a192ebb4af36d447473b19dc87fdf11900b9aa
fahad-zakir <fahadzakir81@gmail.com>
2018-05-26 07:45:08 -0400

    retested

--- client/src/components/NewGift.js
+++ client/src/components/NewGift.js
@@ -18,7 +18,7 @@ class NewGift extends Component {
         let val = event.target.value
         // update the new information
         // and add it to
-        //new test
+        // same git origin test
         const newGift = { ...this.state.newGift }
         newGift[attribute] = val
         newGift.userID = { ...this.state.userID }

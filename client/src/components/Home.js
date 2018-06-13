e449a94aa920892270e38927b788b05e445339a1
fahad-zakir <fahadzakir81@gmail.com>
2018-06-13 06:33:13 -0400

    added images and edited Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,31 +3,38 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
 import UserList from 'styled-components'
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
 
 
 
 class Home extends Component {
     render() {
         return (
-            <Container>
-                <div>
+                <div className="home-bg">
+                <Container>
+
+
+                <Grid>
+                    <Row className="show-grid text-center row">
+                        
+                    <div>
                     <img
                         src="http://pluspng.com/img-png/gift-png-png-file-name-christmas-gift-400.png"
                         alt="gift" className="main-img" />
-                    <div className="models-container">
+                    
+                    
                         <h1>Gifts Wishlist</h1>
-                        <div>
+                    
+                        
                             <Link to="/users">Users</Link>
                         </div>
-                        <div>
-                            <Link to="/gift">Gift</Link>
-                        </div>
-                        <div>
-                            <Link to="/giftInfo">Gift Info</Link>
-                        </div>
-                    </div>
+                    
+                    </Row>
+                </Grid>
+                </Container>
                 </div>
-            </Container>
         )
     }
 }
@@ -35,38 +42,27 @@ class Home extends Component {
 export default Home
 
 
-//background from codepen. User Taylor Vowell
 const Container = styled.div`
-  font-family: 'Lato', sans-serif;
-  font-family: 'Playfair Display', serif;
+  font-family: "Lato", sans-serif;
+  font-family: "Playfair Display", serif;
   font-weight: 300;
-    display: flex;
-    justify-content: center;
-    width:100vw;
-    height:100vh;
-    background: rgb(105, 155, 200);
-    background: -moz - radial - gradient(top left, ellipse cover, rgba(105, 155, 200, 1) 0 %, rgba(181, 197, 216, 1) 57 %);
-    background: -webkit - gradient(radial, top left, 0px, top left, 100 %, color - stop(0 %, rgba(105, 155, 200, 1)), color - stop(57 %, rgba(181, 197, 216, 1)));
-    background: -webkit - radial - gradient(top left, ellipse cover, rgba(105, 155, 200, 1) 0 %, rgba(181, 197, 216, 1) 57 %);
-    background: -o - radial - gradient(top left, ellipse cover, rgba(105, 155, 200, 1) 0 %, rgba(181, 197, 216, 1) 57 %);
-    background: -ms - radial - gradient(top left, ellipse cover, rgba(105, 155, 200, 1) 0 %, rgba(181, 197, 216, 1) 57 %);
-    background: radial - gradient(ellipse at top left, rgba(105, 155, 200, 1) 0 %, rgba(181, 197, 216, 1) 57 %);
-    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr = '#699bc8', endColorstr = '#b5c5d8', GradientType = 1);
-    h1 {
-        font-family: 'Special Elite', cursive, bold;
-        padding-top: 50px;
-    }
+  display: flex;
+  justify-content: center;
 
-    a {
-    text-decoration: none;
-    color: white;
+  h1 {
+    font-family: "Special Elite", cursive, bold;
+    padding-top: 50px;
+  }
+
+  a {
+
+    color: black;
     font-size: 25px;
     z-index: auto;
     &:hover {
-    text-shadow: none;
-    text-shadow:2px 2px 2px #000000;
-    }
-    }
-`
+        color: black;
+text-shadow: 1px 1px 1px, 0 0 25px blue;    }
+  }
+`;
 
 
